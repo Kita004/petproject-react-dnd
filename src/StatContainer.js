889d63afb5c46f79a49e9e2e995da546f23fa87f
Kitti Born <born.kitti97@gmail.com>
2023-01-31 13:41:31 +0100

    refactor placement of tbody tag

--- src/StatContainer.js
+++ src/StatContainer.js
@@ -3,17 +3,17 @@ import React from "react";
 const StatContainer = ({statStates, stats}) => {
     return <div id="statContainer" className="container">
         <table id="statContainerTable" className="characterTable">
+            <tbody>
             {stats.map( stat => {
-                    return <tbody>
-                    <tr>
+                    return <tr>
                         <td>{stat}</td>
                         <td>
                             <input id={stat + "container"} type="number" min="1" max="20" placeholder={statStates[stats.indexOf(stat)].toString()}/>
                         </td>
                     </tr>
-                    </tbody>
                 }
             )}
+            </tbody>
         </table>
     </div>
 }
