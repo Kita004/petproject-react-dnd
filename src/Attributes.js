a5e4a16bfdc8d6e5b6af2064f6340e1a1521f790
Kitti Born <born.kitti97@gmail.com>
2023-01-31 11:11:13 +0100

    add Attributes.js for holding StatSelector and SavingThrowSelector

---
+++ src/Attributes.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Attributes = () => {
+    return <div className="container" id="AttributesContainer">
+        <h2>Attributes</h2>
+        <div style="display: flex" className="flexBox">
+        </div>
+    </div>
+}
+
+export default Attributes;
