5d6dc64b52786384b06f9c8e4f38bd7988e06481
Kitti Born <born.kitti97@gmail.com>
2023-01-31 11:33:07 +0100

    refactor StatSelector.js, based on template.html

--- src/StatSelector.js
+++ src/StatSelector.js
@@ -2,15 +2,24 @@ import React from "react";
 
 const StatSelector = ({stats, nums, handleOptionChange}) => {
     return <div id="statSelector" className="container">
+        <table className="characterTable" id="statTable">
+
+        </table>
             {stats.map(stat => {
-                return <div className={stat + "selector"}>
-                    <select id={stat + "select"} name={stat} className="statSelect" onChange={handleOptionChange}>
-                        <option value="-">--{stat}--</option>
-                        {nums.map(num => {
-                            return <option value={num}>{num}</option>
-                        })}
-                    </select>
-                </div>
+                return <tr>
+                    <td>
+                        {stat}
+                    </td>
+                    <td>
+                        <select id={stat + "select"} name={stat} className="statSelect" onChange={handleOptionChange}>
+                            <option value="-">--{stat}--</option>
+                            {nums.map(num => {
+                                return <option value={num}>{num}</option>
+                            })}
+                        </select>
+                    </td>
+                    <td>**insert calculation**</td>
+                </tr>
             })}
         </div>
 }
