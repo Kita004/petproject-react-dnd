c7f8783efc2ec62cd265a61b27fdcb8d544f87ad
Kitti Born <born.kitti97@gmail.com>
2023-02-01 13:01:28 +0100

    move file and add func for generating random numbers for stats

---
+++ src/utils/formulas.js
@@ -0,0 +1,41 @@
+export function calculateProficiency(level) {
+    return level / 4;
+}
+
+export function calculateMaxHP(hitDice, level, conMOD) {
+    return level * (hitDice / 2 + 1) + conMOD;
+}
+
+export function calculateWithProficiency(stat, modifier) {
+    return stat + modifier;
+}
+
+export function calculateStatMod(stat) {
+    return Math.floor((stat - 10) / 2);
+}
+
+export function roll(die) {
+    return Math.floor(Math.random() * die) +1;
+}
+
+export function rollRandomStats() {
+    const DICE_VALUE = 6
+    const DICE_AMOUNT = 4
+
+    const STAT_AMOUNT = 6
+
+    const statsRolled = []
+
+    for (let i = 0; i < STAT_AMOUNT; i++) {
+        const tempRolls = []
+        const statValue = 0
+
+        for (let j = 0; j < DICE_AMOUNT; j++) {
+            tempRolls.push(Math.floor(Math.random() * DICE_VALUE) + 1)
+        }
+
+        tempRolls.sort(((a,b)=>a-b))
+        statsRolled.push(tempRolls.slice(1).reduce((acc, curr) => acc + curr, statValue))
+    }
+    return statsRolled
+}
