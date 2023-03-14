fd65818f86f905eb1affb3fd8a74a5d5ce42c545
Kitti Born <born.kitti97@gmail.com>
2023-03-14 15:32:23 +0100

    refactor Character

--- backend/src/main/java/com/codecool/backend/model/Character.java
+++ backend/src/main/java/com/codecool/backend/model/Character.java
@@ -1,9 +1,15 @@
 package com.codecool.backend.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
 
 import java.util.Set;
 
+@Getter
+@Setter
 @Entity
 public class Character {
     @Id
@@ -19,12 +25,14 @@ public class Character {
     private String background;
     private String alignment;
 
-    @ManyToOne
+    @OneToOne
     @JoinColumn(name = "stats_id")
+    @Cascade(CascadeType.ALL)
     // TODO: make it individual Entity?
     private Stats stats;
 
     @ElementCollection
+    @Cascade(CascadeType.ALL)
     // TODO: make it individual Entity?
     private Set<String> skills;
 }
