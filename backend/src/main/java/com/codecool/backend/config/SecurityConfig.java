421c647cb69ad5dd9dd43bad3539a155fc4a93d1
Kitti Born <born.kitti97@gmail.com>
2023-03-14 15:15:48 +0100

    add SecurityConfig

---
+++ backend/src/main/java/com/codecool/backend/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.codecool.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpS) throws Exception {
+        return httpS
+                .cors().disable()
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .requestMatchers("/").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .build();
+    }
+}
