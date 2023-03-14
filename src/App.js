fe353e5c11201cf1d733cbc1c27866e5cb045956
Kitti Born <born.kitti97@gmail.com>
2023-03-14 10:00:59 +0100

    refactor StatSelector methods into new charCreation for later use

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import api from "./utils/api";
 
 import StatSelector from "./StatSelector";
@@ -9,18 +9,17 @@ import Header from "./Header";
 import SkillsSelector from "./SkillsSelector";
 import RollPopupButton from "./RollPopupButton";
 
-
-
 function App() {
-    const stats = ["STR", "DEX", "CON", "INT", "WIS", "CHA"]
-    const nums = [8,10,12,13,14,15]
+    const stats = ["STR", "DEX", "CON", "INT", "WIS", "CHA"];
+    const nums = [8, 10, 12, 13, 14, 15];
 
+    const [classDetail, setClassDetail] = useState(null);
+    const [raceDetail, setRaceDetail] = useState(null);
 
-    const [classOptions, setClassOptions] = useState([])
-    const [classDetail, setClassDetail] = useState(null)
-    const [alignmentOptions, setAlignmentOptions] = useState([])
+    const [classOptions, setClassOptions] = useState([]);
+    const [alignmentOptions, setAlignmentOptions] = useState([]);
     const [raceOptions, setRaceOptions] = useState([]);
-    const [backgroundOptions, setBackgroundOptions] = useState([])
+    const [backgroundOptions, setBackgroundOptions] = useState([]);
 
     const [STR, setSTR] = useState(8);
     const [DEX, setDEX] = useState(10);
@@ -29,9 +28,7 @@ function App() {
     const [WIS, setWIS] = useState(14);
     const [CHA, setCHA] = useState(15);
 
-    const statStates = [STR, DEX, CON, INT, WIS, CHA]
-
-    const selects = document.getElementsByClassName('statSelect');
+    const statStates = [STR, DEX, CON, INT, WIS, CHA];
 
     const [STRsaving, setSTRsaving] = useState(false);
     const [DEXsaving, setDEXsaving] = useState(false);
@@ -40,102 +37,135 @@ function App() {
     const [WISsaving, setWISsaving] = useState(false);
     const [CHAsaving, setCHAsaving] = useState(false);
 
-    const savingThrowStates = [STRsaving, DEXsaving, CONsaving, INTsaving, WISsaving, CHAsaving];
+    const savingThrowStates = [
+        STRsaving,
+        DEXsaving,
+        CONsaving,
+        INTsaving,
+        WISsaving,
+        CHAsaving,
+    ];
 
-    const [charName, setCharName] = useState('')
-    const [charLevel, setCharLevel] = useState('')
-    const [charClass, setCharClass] = useState('')
-    const [charSubClass,setCharSubClass] = useState('')
-    const [charRace, setCharRace] = useState('')
-    const [charBackground, setCharBackground] = useState('')
-    const [charAlignment, setCharAlignment] = useState('')
-
+    const [charName, setCharName] = useState("");
+    const [charLevel, setCharLevel] = useState("");
+    const [charClass, setCharClass] = useState("");
+    const [charSubClass, setCharSubClass] = useState("");
+    const [charRace, setCharRace] = useState("");
+    const [charBackground, setCharBackground] = useState("");
+    const [charAlignment, setCharAlignment] = useState("");
 
     useEffect(() => {
         fetchCharacter();
         fetchDndAPI();
         fetchClassDetail();
-    }, [])
+        fetchRaceDetail();
+    }, []);
 
     useEffect(() => {
         fetchClassDetail();
-    }, [charClass])
+    }, [charClass]);
 
+    useEffect(() => {
+        fetchRaceDetail();
+    }, [charRace]);
 
     const fetchClassDetail = async () => {
         try {
-            const resClassDetail = await api.get('https://www.dnd5eapi.co/api/classes/' + charClass)
-            setClassDetail(resClassDetail.data)
-            setSavingThrowStates(resClassDetail.data.saving_throws)
+            const resClassDetail = await api.get(
+                "https://www.dnd5eapi.co/api/classes/" + charClass
+            );
+            setClassDetail(resClassDetail.data);
+            setSavingThrowStates(resClassDetail.data.saving_throws);
         } catch (e) {
             if (e.response) {
-                console.log(e.response.data)
+                console.log(e.response.data);
             } else {
-                console.log("ERROR: " + e)
+                console.log("ERROR: " + e);
             }
         }
-    }
+    };
+
+    const fetchRaceDetail = async () => {
+        try {
+            const resRaceDetail = await api.get(
+                "https://www.dnd5eapi.co/api/races/" + charRace
+            );
+            setRaceDetail(resRaceDetail.data);
+        } catch (e) {
+            if (e.response) {
+                console.log(e.response.data);
+            } else {
+                console.log("ERROR: " + e);
+            }
+        }
+    };
 
     const fetchCharacter = async () => {
         try {
-            const res = await api.get('/character')
-            setCharacterStates(res.data)
+            const res = await api.get("/character");
+            setCharacterStates(res.data);
         } catch (e) {
             if (e.response) {
-                console.log(e.response.data)
+                console.log(e.response.data);
             } else {
-                console.log("ERROR: " + e)
+                console.log("ERROR: " + e);
             }
         }
-    }
+    };
 
     const fetchDndAPI = async () => {
         try {
             // fetch classes for Class Select
-            const resClasses = await api.get('https://www.dnd5eapi.co/api/classes')
-            setClassOptions(resClasses.data.results)
+            const resClasses = await api.get(
+                "https://www.dnd5eapi.co/api/classes"
+            );
+            setClassOptions(resClasses.data.results);
 
             // fetch alignments for Alignment Select
-            const resAlignments = await api.get('https://www.dnd5eapi.co/api/alignments')
-            setAlignmentOptions(resAlignments.data.results)
+            const resAlignments = await api.get(
+                "https://www.dnd5eapi.co/api/alignments"
+            );
+            setAlignmentOptions(resAlignments.data.results);
 
             // fetch classes for Class Select
-            const resRaces = await api.get('https://www.dnd5eapi.co/api/races')
-            setRaceOptions(resRaces.data.results)
+            const resRaces = await api.get("https://www.dnd5eapi.co/api/races");
+            setRaceOptions(resRaces.data.results);
 
             // fetch Backgrounds for Background Select
-            const resBg = await  api.get('https://www.dnd5eapi.co/api/backgrounds')
-            setBackgroundOptions(resBg.data.results)
+            const resBg = await api.get(
+                "https://www.dnd5eapi.co/api/backgrounds"
+            );
+            setBackgroundOptions(resBg.data.results);
         } catch (e) {
             if (e.response) {
-                console.log(e.response.data)
+                console.log(e.response.data);
             } else {
-                console.log("ERROR: " + e)
+                console.log("ERROR: " + e);
             }
         }
-    }
+    };
 
     const setCharacterStates = (character) => {
-        setCharName(character.name)
-        setCharClass(character.class)
-        setCharSubClass(character.subClass)
-        setCharRace(character.race)
-        setCharLevel(character.level)
-        setCharBackground(character.background)
-        setCharAlignment(character.alignment)
-    }
+        setCharName(character.name);
+        setCharClass(character.class);
+        setCharSubClass(character.subClass);
+        setCharRace(character.race);
+        setCharLevel(character.level);
+        setCharBackground(character.background);
+        setCharAlignment(character.alignment);
+    };
 
     const setSavingThrowStates = (savingThrows) => {
         let savingThrowNames = [];
 
         // get names from Array of Objects
         for (let st of savingThrows) {
-            savingThrowNames.push(st.name)
+            savingThrowNames.push(st.name);
         }
 
         // set all states to false, beforehand
         for (let stat of stats) {
-            changeState(stat + "saving", false)
+            changeState(stat + "saving", false);
         }
 
         // set state to true according to condition
@@ -144,88 +174,79 @@ function App() {
                 changeState(stat + "saving", true);
             }
         }
-    }
-
-    const handleStatChange = (e) => {
-        const statId = e.target.id;
-        const statName = e.target.name;
-        const statValue = e.target.value;
-        const valueToRepair = document.getElementById(statId).value;
-
-
-        removeOption(statId, statValue);
-        repairOption(valueToRepair);
-        changeState(statName, statValue);
-    }
-
-    const removeOption = (statId, statValue) => {
-        // if statement so that default value does not remove default option
-        if (statValue !== "-") {
-            for (let i = 0; i < selects.length; i++) {
-                if (selects[i].id !== statId) {
-                    for (let j = 0; j < selects[i].length; j++) {
-                        if (selects[i][j].value === statValue) {
-                            selects[i][j].style.display = "none";
-                        }}}}}
-    }
-
-    const repairOption = (statToRepair) => {
-        for (let i = 0; i < selects.length; i++) {
-            for (let j = 0; j < selects[i].length; j++) {
-                if (selects[i][j].value === statToRepair) {
-                    selects[i][j].style.display = "block";
-                }}}
-    }
-
-
+    };
 
     const changeState = (statName, statValue) => {
         switch (statName) {
             case "STR":
-                setSTR(statValue)
-                break
+                setSTR(statValue);
+                break;
             case "DEX":
-                setDEX(statValue)
-                break
+                setDEX(statValue);
+                break;
             case "CON":
-                setCON(statValue)
-                break
+                setCON(statValue);
+                break;
             case "INT":
-                setINT(statValue)
-                break
+                setINT(statValue);
+                break;
             case "WIS":
-                setWIS(statValue)
-                break
+                setWIS(statValue);
+                break;
             case "CHA":
-                setCHA(statValue)
-                break
+                setCHA(statValue);
+                break;
             case "STRsaving":
-                setSTRsaving(statValue)
-                break
+                setSTRsaving(statValue);
+                break;
             case "DEXsaving":
-                setDEXsaving(statValue)
-                break
+                setDEXsaving(statValue);
+                break;
             case "CONsaving":
-                setCONsaving(statValue)
-                break
+                setCONsaving(statValue);
+                break;
             case "INTsaving":
-                setINTsaving(statValue)
-                break
+                setINTsaving(statValue);
+                break;
             case "WISsaving":
-                setWISsaving(statValue)
-                break
+                setWISsaving(statValue);
+                break;
             case "CHAsaving":
-                setCHAsaving(statValue)
-                break
+                setCHAsaving(statValue);
+                break;
             default:
-                throw new Error("Error with switch case, ran into default")
+                throw new Error("Error with switch case, ran into default");
         }
-    }
+    };
+
+    const createCharacter = () => {
+        const newChar = {
+            name: charName,
+            level: charLevel,
+            class: charClass,
+            subClass: charSubClass,
+            race: charRace,
+            background: charBackground,
+            alignment: charAlignment,
+            stats: {
+                str: STR,
+                dex: DEX,
+                con: CON,
+                int: INT,
+                wis: WIS,
+                cha: CHA,
+            },
+            skills: ["to be implemented"],
+        };
+        console.log("new char: " + newChar);
+    };
 
     // render
     return (
         <div className="App">
-            <Header />
+            <Header
+            /*createCharacter={createCharacter}*/
+            />
             <div className="container">
                 <div>
                     <BasicInfoSelector
@@ -235,8 +256,6 @@ function App() {
                         alignmentOptions={alignmentOptions}
                         raceOptions={raceOptions}
                         backgroundOptions={backgroundOptions}
-
-
                         charName={charName}
                         charClass={charClass}
                         charSubClass={charSubClass}
@@ -245,11 +264,18 @@ function App() {
                         charBackground={charBackground}
                         charAlignment={charAlignment}
 
+                        setCharName={setCharName}
                         setCharLevel={setCharLevel}
                         setCharClass={setCharClass}
+                        setCharRace={setCharRace}
+                        setCharAlignment={setCharAlignment}
+                        setCharBackground={setCharBackground}
                     />
                     <h2>Attributes</h2>
-                    <div id="attributesContainer" className="container attributes">
+                    <div
+                        id="attributesContainer"
+                        className="container attributes"
+                    >
                         <StatContainer
                             stats={stats}
                             statStates={statStates}
@@ -259,7 +285,6 @@ function App() {
                             stats={stats}
                             statStates={statStates}
                             savingThrowStates={savingThrowStates}
-
                             charLevel={charLevel}
                         />
                     </div>
@@ -267,16 +292,18 @@ function App() {
                 <SkillsSelector
                     stats={stats}
                     statStates={statStates}
+                    classDetail={classDetail}
+                    raceDetail={raceDetail}
                 />
             </div>
-            <div>
-                <h2>Dice Roller</h2>
-                <RollPopupButton
+            {/*<div>*/}
+            {/*    <h2>Dice Roller</h2>*/}
+            {/*    <RollPopupButton*/}
 
-                />
-                <button>2</button>
-                <button>3</button>
-            </div>
+            {/*    />*/}
+            {/*    <button>2</button>*/}
+            {/*    <button>3</button>*/}
+            {/*</div>*/}
         </div>
     );
 }
