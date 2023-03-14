36439ca1d42ab466c2a907c980eb10366b715dbe
Kitti Born <born.kitti97@gmail.com>
2023-03-14 12:15:15 +0100

    add backend

---
+++ backend/src/main/java/com/codecool/backend/DnDCharacterSheetApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnDCharacterSheetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DnDCharacterSheetApplication.class, args);
+	}
+
+}
