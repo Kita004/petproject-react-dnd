f080d78600fea47674847ec1f7c0da1a81ce7945
Kitti Born <born.kitti97@gmail.com>
2023-03-15 10:00:21 +0100

    comment out security for testing

--- backend/src/main/java/com/codecool/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/codecool/backend/config/SecurityConfig.java
@@ -1,24 +1,25 @@
-package com.codecool.backend.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig {
-
-    @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity httpS) throws Exception {
-        return httpS
-                .cors().disable()
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/h2-console/**").permitAll()
-                        .requestMatchers("/").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .build();
-    }
-}
+//package com.codecool.backend.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig {
+//
+//    @Bean
+//    SecurityFilterChain securityFilterChain(HttpSecurity httpS) throws Exception {
+//        return httpS
+//                .csrf().disable()
+//                .cors().disable()
+//                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers("/h2-console/**").permitAll()
+//                        .requestMatchers("/").permitAll()
+//                        .anyRequest().authenticated()
+//                )
+//                .build();
+//    }
+//}
