3af6504fcdd77b54c5c09ac536cb13c1bb15a4ff
Kitti Born <born.kitti97@gmail.com>
2023-03-15 10:43:01 +0100

    add Controllers

---
+++ backend/src/main/java/com/codecool/backend/api/CharacterController.java
@@ -0,0 +1,45 @@
+package com.codecool.backend.api;
+
+import com.codecool.backend.model.Character;
+import com.codecool.backend.service.CharacterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/characters")
+public class CharacterController {
+    private CharacterService characterService;
+
+    @Autowired
+    public CharacterController(CharacterService characterService) {
+        this.characterService = characterService;
+    }
+
+
+    @GetMapping
+    public List<Character> getAllCharacters() {
+        return characterService.getAllCharacters();
+    }
+
+    @GetMapping("/{id}")
+    private Character getCharacterById(@PathVariable("id") Long id) {
+        return characterService.getCharacterById(id);
+    }
+
+    @PostMapping
+    public void addCharacter(@RequestBody Character newCharacter) {
+        characterService.createCharacter(newCharacter);
+    }
+
+    @PutMapping("/{id}")
+    public void updateCharacterById(@PathVariable("id") Long id, @RequestBody Character updatedCharacter) {
+        characterService.updateCharacterById(id, updatedCharacter);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCharacterById(@PathVariable("id") Long id) {
+        characterService.deleteCharacterById(id);
+    }
+}
---
+++ backend/src/main/java/com/codecool/backend/api/UserController.java
@@ -0,0 +1,45 @@
+package com.codecool.backend.api;
+
+import com.codecool.backend.model.User;
+import com.codecool.backend.service.UserService;
+import jakarta.persistence.Table;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return  userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable("id") Long id) {
+        return userService.getUserById(id);
+    }
+
+    @PostMapping
+    public void addUser(@RequestBody User newUser) {
+        userService.createUser(newUser);
+    }
+
+    @PutMapping("/{id}")
+    public void updateUserById(@PathVariable("id") Long id, @RequestBody User updatedUser) {
+        userService.updateUserById(id, updatedUser);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUserById(@PathVariable("id") Long id) {
+        userService.deleteUserById(id);
+    }
+}
