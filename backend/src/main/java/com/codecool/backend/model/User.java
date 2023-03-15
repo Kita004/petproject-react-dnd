ab71ba3a77078e497f4f27cd7f0dd8a350581c1b
Kitti Born <born.kitti97@gmail.com>
2023-03-15 10:42:39 +0100

    add @Table and name field

--- backend/src/main/java/com/codecool/backend/model/User.java
+++ backend/src/main/java/com/codecool/backend/model/User.java
@@ -9,10 +9,13 @@ import java.util.Set;
 @Getter
 @Setter
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    private String name;
 
     @OneToMany
     @JoinColumn(name = "character_id")
