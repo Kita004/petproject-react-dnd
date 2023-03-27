1fc4113af67b0b5d9da8c83830691ee004b2c049
Kitti Born <born.kitti97@gmail.com>
2023-03-27 11:45:41 +0200

    refactor Component, add look and basic login logic with localstorage usage

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,10 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
+import api from "../utils/api";
 
-const Login = () => {
-    return <div>
+const Login = ({setUser, setUserCharacters}) => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword]= useState("");
+
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+
+        const user = {username, password};
+        const res = await api.post("/api/login", user);
+
+        setUser(res.data);
+        setUserCharacters(res.data.characters);
+
+        localStorage.setItem('user', res.data)
+    }
+
+    return <div id="login-page" className="main-content">
         <h1>
-            This is The Login Page
+            Please Login!
         </h1>
+        <form>
+            <label htmlFor="username">Username: </label>
+            <input
+                type="text"
+                value={username}
+                placeholder="Enter Username"
+                onChange={({target}) => setUsername(target.value)}
+            />
+            <label htmlFor="password">Password:</label>
+            <input
+                type="password"
+                value={password}
+                onChange={({target}) => setPassword(target.value)}
+            />
+            <button type="submit" onSubmit={event => handleSubmit(event)}>Login!</button>
+        </form>
     </div>
 }
 
